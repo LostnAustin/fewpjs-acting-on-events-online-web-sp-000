5e40897444eca50575841dd3f55d3cbf3124f8e3
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-06 04:41:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace('px', '');
+  let rightNumbers = dodger.style.left.replace('px', '');
     let right = parseInt(rightNumbers, 10);
     if (right < 100) {
-    dodger.style.right = `${right - 1}px`;
+    dodger.style.right = `${left - 1}px`;
     }
 }
 
