2ba71f3089a4e3ddeade39540e195a7a00b18801
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-06 04:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace('px', '');
-    let right = parseInt(leftNumbers, 10);
+    let right = parseInt(rightNumbers, 10);
     if (right < 100) {
     dodger.style.right = `${right - 1}px`;
     }
